5cd023ae243e47c5239a671d00166e04a9eaf9bb
Francisco Peixoto <thtr.irubra@gmail.com>
2021-11-04 09:01:31 +0000

    breve introdução ao tema
    criação de quatro classes iniciais
    > cliente / servidor
    > file transfer / sincronização

---
+++ src/FFSync.java
@@ -0,0 +1,15 @@
+/**
+ *
+ *  PROTOCOLO DE SINCRONIZAÇÃO
+ *
+ *  Objetivo:
+ *      - no protocolo devem estar definidas medidas para o comportamento da sincronização
+ *      - os metodos de sincronização devem ter devidas regras estipuladas (tempo + processo)
+ *      - o protocolo de sincronização faz recurso do protocolo de transferência de dados
+ *      -
+ *
+ * */
+
+public class FFSync {
+
+}
---
+++ src/FTRapid.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * PROTOCOLO DE TRANSFERÊNCIA DE DADOS
+ *
+ * Objetivo:
+ *      - formular um cabeçalho próprio
+ *      - realizar transferência de ficheiros
+ *      - 
+ *
+ * */
+
+public class FTRapid {
+
+}
