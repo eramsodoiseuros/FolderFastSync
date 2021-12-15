980179e67f5272ecd6a4e17726b42c046d1d4b49
Samuel de Almeida Simoes Lira <sdasl03@github.com>
2021-12-15 11:36:54 +0000

    folderparser metadata

--- src/folder_parser/FolderParser.java
+++ src/folder_parser/FolderParser.java
@@ -1,6 +1,7 @@
 package folder_parser;
 
 import app.FFSync;
+import ffrapid_protocol.packet.Metadata;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -11,6 +12,10 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static common.debugger.Debugger.log;
 //import org.json;
 
 /**
@@ -130,7 +135,8 @@ public class FolderParser {
         }
 
     public static JSONObject listar(){
-        File directoryPath = new File(System.getProperty("user.dir"));
+        String dirName= "user.home";
+        File directoryPath = new File(System.getProperty(dirName));
         JSONObject obj = new JSONObject();
         File[] filesList = directoryPath.listFiles();
 
@@ -138,18 +144,52 @@ public class FolderParser {
 
             for(File file : filesList) {
                 JSONArray ficheiro = new JSONArray();
-                ficheiro.add("file name: "+file.getName()+" ; ");
+                ficheiro.add("file name: "+file.getName()+" ; ");//duas vezes
                 ficheiro.add("file path: "+file.getAbsolutePath()+" ; ");
                 LocalDateTime d = LocalDateTime.ofInstant(Instant.ofEpochMilli(file.lastModified()), ZoneId.systemDefault());
                 ficheiro.add("file last update: "+d+" ; ");
                 ficheiro.add("file size: "+file.getTotalSpace()+" bytes; ");
+                //adicionar linha?
                 obj.put("Name: "+file.getName(),ficheiro);
-
             }
             System.out.println(obj);
         }
         return obj;
     }
+    //em teste
+    public void listSubDir(File f,List<File> lf){
+        File[] files=f.listFiles();
+        for(File f1:files) {
+            if (f1.isFile()) {
+                lf.add(f1);
+            } else if (f.isDirectory()) {
+                listSubDir(f, lf);
+            }
+        }
+    }
+    //em teste
+    public List<Map.Entry<String, Long>> compareMetadata1(Metadata metadata) {
+        File files[] = FFSync.getCurrentDirectory().listFiles();
+        List<File> lf= new ArrayList<>();
+
+        Map<String, Long> filesMeta = new HashMap();
+
+        for(File f: files)
+            listSubDir(f,lf);
+
+        for(File f: lf)
+            filesMeta.put(f.getPath(),f.lastModified());
+
+        Predicate<Map.Entry<String, Long>> different = e -> {
+            Long time = filesMeta.get(e.getKey());
+            return !Objects.equals(time, e.getValue()) || time == null;
+        };
+        log("Local files: " + filesMeta);
+        log("Other files: " + metadata.metadata);
+        return metadata.metadata.entrySet().stream().filter(different).collect(Collectors.toList());
+    }
+    //em teste
+
     public static void main(String[] args) {
         /*
         FolderParser fp = new FolderParser();
---
+++ src/test/folderparser/FolderParserTest.java
@@ -0,0 +1,20 @@
+package test.folderparser;
+
+import ffrapid_protocol.packet.Metadata;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class FolderParserTest {
+
+    @org.junit.jupiter.api.Test
+    void listSubDir() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareMetadata1() {
+        Map<Object, Object> map = new HashMap<>();
+        Metadata metadata = new Metadata(files);
+
+    }
+}
