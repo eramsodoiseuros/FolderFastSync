d54178d48bc5f8f102c37c511b1a160dbf79288d
Samuel de Almeida Simoes Lira <sdasl03@github.com>
2021-12-19 08:50:35 +0000

    folderparser pequenas mudancas

--- src/test/folderparser/FolderParserTest.java
+++ src/test/folderparser/FolderParserTest.java
@@ -1,8 +1,11 @@
 package test.folderparser;
 
 import ffrapid_protocol.packet.Metadata;
+import folder_parser.FolderParser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class FolderParserTest {
@@ -13,8 +16,14 @@ class FolderParserTest {
 
     @org.junit.jupiter.api.Test
     void compareMetadata1() {
-        Map<Object, Object> map = new HashMap<>();
-        Metadata metadata = new Metadata(files);
+        String[] strings={"/.gitignore","/CC-Enunciado-TP2-2022.pdf"};
+        List<String> ls=new ArrayList<>();
+        for(String s: strings){
+            ls.add(s);
+        }
+        Metadata metadata = Metadata.getMetadataFromNames(ls);
+        FolderParser fp= new FolderParser();
+        fp.compareMetadata1(metadata);
 
     }
 }
