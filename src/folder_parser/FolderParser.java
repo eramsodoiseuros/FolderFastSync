73b0bc8cb4d59e6c4d9ff5910cbbed8b73d6f641
Samuel de Almeida Simoes Lira <sdasl03@github.com>
2021-12-21 20:45:55 +0000

    comentarios

--- src/folder_parser/FolderParser.java
+++ src/folder_parser/FolderParser.java
@@ -1,9 +1,10 @@
 package folder_parser;
 
 import app.FFSync;
+/*
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
+*/
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
